cdf1ec3ae956f66e64d53f47f21520cef60bd502
DarKRs <centralstreet_24@mail.ru>
2024-11-04 15:10:04 +0600

    Проверка событий

--- AppForTest/MainPage.xaml.cs
+++ AppForTest/MainPage.xaml.cs
@@ -10,6 +10,20 @@ namespace AppForTest
         {
             InitializeComponent();
             //Calendar.Culture = CultureInfo.GetCultureInfo("fr-FR");
+            Calendar.DateSelected += OnDateSelected;
+            Calendar.DateRangeSelected += OnDateRangeSelected;
+        }
+
+        private void OnDateSelected(object sender, DateTime selectedDate)
+        {
+            // Обновляем Label с выбранной датой
+            SelectedDateLabel.Text = $"Вы выбрали дату: {selectedDate:d}";
+        }
+
+        private void OnDateRangeSelected(object sender, (DateTime, DateTime) dateRange)
+        {
+            // Обновляем Label с выбранным диапазоном дат
+            DateRangeLabel.Text = $"Вы выбрали диапазон: {dateRange.Item1:d} - {dateRange.Item2:d}";
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
